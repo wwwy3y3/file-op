21067992b3c79cda23ee5af27b0af0823b49a9b1
william <wwwy3y3@gmail.com>
2014-12-26 12:21:12 +0800

    remove ok

--- test/fs.js
+++ test/fs.js
@@ -1,9 +1,18 @@
 var fop= require('../');
 var fs= require('fs');
 var testfile= process.cwd()+'/test/test.txt';
-
-fop.insert(testfile, 'qwe', 0)
+/*
+fop.insert(testfile, '123', 3)
    .done(function () {
+   		var content= fs.readFileSync(testfile, 'utf8');
+   		console.log(content);
+   }, function (err) {
+   	console.log(err);
+   })
+*/
+
+fop.remove(testfile, 5,8)
+	.done(function () {
    		var content= fs.readFileSync(testfile, 'utf8');
    		console.log(content);
    }, function (err) {
