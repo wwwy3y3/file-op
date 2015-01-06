22bb747ffc3769c9f4004e4b3798d59bbc92b366
william <wwwy3y3@gmail.com>
2015-01-06 20:25:28 +0800

    test

--- test/fs.js
+++ test/fs.js
@@ -1,16 +1,18 @@
 var fop= require('../');
 var fs= require('fs');
 var testfile= process.cwd()+'/test/test.txt';
-/*
-fop.insert(testfile, '123', 3)
+
+// insert test
+fop.insert(testfile, '456\n22', 0)
    .done(function () {
    		var content= fs.readFileSync(testfile, 'utf8');
    		console.log(content);
    }, function (err) {
    	console.log(err);
    })
-*/
 
+
+//remove test
 fop.remove(testfile, 0,2)
 	.done(function () {
    		var content= fs.readFileSync(testfile, 'utf8');
